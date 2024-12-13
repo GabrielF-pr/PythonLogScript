f2caa6e3a8f47542f920537fc22eca0f7822d1a7
Gabriel <gabrielalcantara2705@gmail.com>
2024-12-13 22:46:01 +0000

    Added color to the keyword

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ def main():
         matches = parse_logs(log_file, keyword)
         print(f"Found {len(matches)} matching lines:")
         for match in matches:
-            print(match)
+            for word in match.split():
+                if word in keyword.split():
+                    print("\033[91m {}\033[00m".format(word), end=" ")
+                else:
+                    print(word, end=" ")
+            print("")
     else:
          print("Log file not found.")
 
